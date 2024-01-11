ed9542d55e057d9064a8fa85d1ec3cca6143e88c
Yohe-Am <56622350+Yohe-Am@users.noreply.github.com>
2024-01-11 01:48:55 +0000

    fix: stale comment

--- ghjk.ts
+++ ghjk.ts
@@ -2,7 +2,6 @@ export { ghjk } from "https://raw.github.com/metatypedev/ghjk/b8935ba8/mod.ts";
 import * as ghjk from "https://raw.github.com/metatypedev/ghjk/b8935ba8/mod.ts";
 import * as ports from "https://raw.github.com/metatypedev/ghjk/b8935ba8/ports/mod.ts";
 
-// these are used for developing ghjk
 ghjk.install(
   ports.act(),
 );
