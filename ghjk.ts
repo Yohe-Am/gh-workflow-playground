1961c8c7e9371c22a612a2f11328671a5f166c58
Yohe-Am <56622350+Yohe-Am@users.noreply.github.com>
2024-01-11 01:48:29 +0000

    feat: `ghjk.ts`

---
+++ ghjk.ts
@@ -0,0 +1,8 @@
+export { ghjk } from "https://raw.github.com/metatypedev/ghjk/b8935ba8/mod.ts";
+import * as ghjk from "https://raw.github.com/metatypedev/ghjk/b8935ba8/mod.ts";
+import * as ports from "https://raw.github.com/metatypedev/ghjk/b8935ba8/ports/mod.ts";
+
+// these are used for developing ghjk
+ghjk.install(
+  ports.act(),
+);
